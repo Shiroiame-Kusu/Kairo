04ea2e9d2745baf5da60e46dc2fb98201952c52d
楠ちゃん <kusu@kusu.moe>
2025-10-20 10:50:24 +0800

    Update Global.cs

--- Kairo/Global.cs
+++ Kairo/Global.cs
@@ -17,7 +17,7 @@ namespace Kairo
         public static bool LoginedByConsole = false;
         public const string Version = "3.1.1";
         public const string VersionName = "Iris Bloom";
-        public const string Branch = "Beta";
+        public const string Branch = "Release";
         public const int Revision = 1;
         public static readonly BuildInfo BuildInfo = new();
         public const string Developer = "Shiroiame-Kusu & Daiyangcheng";
