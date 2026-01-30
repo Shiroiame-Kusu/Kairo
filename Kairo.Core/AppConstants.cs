c1e31ab1acdd7d5d8d1558bac535825a9798e212
楠ちゃん <kusu@kusu.moe>
2026-01-30 22:51:02 +0800

    Ver 3.3.0 "Stellar Rhapsody"

--- Kairo.Core/AppConstants.cs
+++ Kairo.Core/AppConstants.cs
@@ -6,7 +6,7 @@ namespace Kairo.Core;
 public static class AppConstants
 {
     public const string Version = "3.3.0";
-    public const string VersionName = "";
+    public const string VersionName = "Stellar Rhapsody";
     public const string Branch = "Alpha";
     public const int Revision = 4;
     public const string Developer = "Shiroiame-Kusu & Daiyangcheng";
