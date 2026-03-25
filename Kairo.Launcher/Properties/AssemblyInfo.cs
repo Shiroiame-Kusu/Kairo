599e7de5c59573753ca41b8e64f6aaa92d9689ff
楠ちゃん <kusu@kusu.moe>
2026-03-25 14:05:05 +0800

    Well

---
+++ Kairo.Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Kairo.Core;
+
+[assembly: AssemblyTitle("Kairo")]
+[assembly: AssemblyProduct("Kairo")]
+[assembly: AssemblyDescription("A tool which can launch LocyanFrp's proxies.")]
+[assembly: AssemblyCompany(AppConstants.Developer)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(AppConstants.Version + ".0")]
+[assembly: AssemblyFileVersion(AppConstants.Version + ".0")]
+[assembly: AssemblyInformationalVersion(AppConstants.Version + " " + AppConstants.VersionName)]
